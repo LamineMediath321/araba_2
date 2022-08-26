3bce453168d2e8a010e36eefe30a7fa03257b46a
LamineMediath321 <89913597+LamineMediath321@users.noreply.github.com>
2022-08-26 02:13:31 +0200

    Rendre dynamique les categorie puis la page acheter

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
